53fe1f4cfcb44995de1e89552e2e8ad92bd72822
404none <rzeznik997@gmail.com>
2017-06-24 15:45:34 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -13,37 +13,11 @@
 			});
 		});
 
-/* Hamburger menu */
-
 		$(function() { 
 			$(".navbar-toggle").on("click", function () {
     		$(this).toggleClass("active");
 			});
 		});
-
-/* Scroll spy */
-		
-    // $(function () {
-    //     var sectionsToWatch = [];
-    //     var menuItems = $('.navbar-right').eq(0).find('li');
-    //     $(menuItems).each(function () {          sectionsToWatch.push($(this).find('a').attr('href').replace('#', ''));
-    //     });
- 
-    //     $(document).scroll(function () {
-    //         var $nav = $(".navbar-fixed-top");
-    //         $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
- 
-    //         var scrollTopOffset = $(document).scrollTop();
- 
-    //         $(sectionsToWatch).each(function (k, v) {
-    //             if ($('#' + v).length && scrollTopOffset >= $('#' + v).offset().top - $('.navbar-fixed-top').height()) {
-    //                 $('.navbar-right li').removeClass('active');
-    //                 var x = $('.navbar-right li').find('a[href="#' + v + '"]');
-    //                 x.parent('li').addClass('active');
-    //             }
-    //         }); 
-    //     });
-    // });
 
 
     $(document).ready(function() {
@@ -52,13 +26,6 @@
     });
 });
 
-// var viewMoreGallery = document.querySelector('.viewMoreGallery');
-
-// viewMoreGallery.addEventListener('click', function(){
-//     var hidden = document.querySelector('.hidden-content-gallery');
-//     hidden.hide('hidden');
-
-// }, false);
 
 $(document).ready(function(){
     $('.viewMoreGallery').click(function(event){
